7bbc379cc067c7c2486caf9be7dc14f05da21f8d
maged mohamed <maged12345@Github.com>
2023-12-09 10:57:12 +0200

    Initial Commit

---
+++ Metchik/MetchikApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetchikApp.swift
+//  Metchik
+//
+//  Created by s on 09/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct MetchikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
