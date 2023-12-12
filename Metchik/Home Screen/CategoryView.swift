fde1bdcfca1635fcacd2ca25e4bd70cf5751f240
hassan sayed <faculty@Hassans-MacBook-Pro.local>
2023-12-12 22:29:55 +0200

    create category view

--- Metchik/Home Screen/CategoryView.swift
+++ Metchik/Home Screen/CategoryView.swift
@@ -1,42 +1,11 @@
 //
-//  ContentView.swift
+//  CategoryView.swift
 //  Metchik
 //
-//  Created by s on 09/12/2023.
+//  Created by Faculty on 12/12/2023.
 //
 
 import SwiftUI
-
-struct HomeView: View {
-    let categories = ["Men", "Women", "Kids"]
-    var body: some View {
-        
-        ScrollView {
-            VStack {
-                ForEach(0..<3) { index in
-                    NavigationLink {
-                        CategoryView(categorie: categories[index])
-                    } label: {
-                        VStack {
-                            Image(categories[index])
-                            Text(categories[index])
-                        }
-                    }
-                }
-            }
-            .navigationTitle("Metchik 👕👚")
-            
-        }
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    
-    static var previews: some View {
-        HomeView()
-    }
-}
-
 
 
 struct CategoryView: View {
@@ -70,4 +39,9 @@ struct CategoryView: View {
         }
     }
     
+}
+
+
+#Preview {
+    CategoryView(categorie: "Men")
 }
