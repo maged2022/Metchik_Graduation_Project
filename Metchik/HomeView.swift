bc7dff5ddf7faa11e2d0e744d3566092c67101ad
hassan sayed <faculty@Hassans-MacBook-Pro.local>
2023-12-12 22:21:45 +0200

    refactor home screen code
    i removed the category class and replace it with global one

--- Metchik/HomeView.swift
+++ Metchik/HomeView.swift
@@ -8,51 +8,30 @@
 import SwiftUI
 
 struct HomeView: View {
+    let categories = ["Men", "Women", "Kids"]
     var body: some View {
         
-        
-            ScrollView {
-                VStack {
+        ScrollView {
+            VStack {
+                ForEach(0..<3) { index in
                     NavigationLink {
-                        MenCategoryView()
+                        CategoryView(categorie: categories[index])
                     } label: {
                         VStack {
-                            Image("men")
-                            Text("Men")
-                        }
-                       
-                    }
-                    
-                    NavigationLink {
-                        WomenCategoryView()
-                    } label: {
-                        VStack {
-                            Image("women")
-                            Text("Women")
+                            Image(categories[index])
+                            Text(categories[index])
                         }
                     }
-                    
-                    NavigationLink {
-                        
-                        KidCategoryView()
-                    } label: {
-                        VStack {
-                            Image("kid")
-                            Text("Kid")
-                        }
-                    }
-
                 }
-           
-            
+            }
             .navigationTitle("Metchik 👕👚")
-           
+            
         }
-    
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    
     static var previews: some View {
         HomeView()
     }
@@ -60,252 +39,35 @@ struct ContentView_Previews: PreviewProvider {
 
 
 
-
-struct MenCategoryView: View {
-    let categories = ["men", "women", "kid"] // Define your categories here
-
+struct CategoryView: View {
+    @State var categorie: String
+    let productType: [String] = ["Shoes" ,"Clothing" ,"Accessories"]
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
-            
-                    
+                
+                ForEach(0..<3) { outerIndex in
                     VStack(alignment: .leading) {
-                        Text("Men_Shoes")
+                        Text("\(categorie) \(productType[outerIndex])")
                             .font(.headline)
                             .padding(.horizontal)
-
+                        
                         ScrollView(.horizontal, showsIndicators: true) {
                             HStack(spacing: 10) {
                                 ForEach(0..<10) { index in
-                                    Image("Men_Shoes") // Use actual image names for each category
+                                    Image("\(categorie) \(productType[outerIndex])")
                                         .resizable()
                                         .aspectRatio(contentMode: .fit)
                                         .frame(width: 200, height: 200)
-                                        // Use actual image names or URLs for each category
-                                        // Example: Image("clothesImageName")
                                 }
                             }
                             .padding(.horizontal)
                         }
                     }
-                   
-                // Second Category
-                VStack(alignment: .leading) {
-                    Text("Men_Clothing")
-                        .font(.headline)
-                        .padding(.horizontal)
-
-                    ScrollView(.horizontal, showsIndicators: true) {
-                        HStack(spacing: 10) {
-                            ForEach(0..<10) { index in
-                                Image("Men_Clothing") // Use actual image names for each category
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 200, height: 200)
-                                    // Use actual image names or URLs for each category
-                                    // Example: Image("clothesImageName")
-                            }
-                        }
-                        .padding(.horizontal)
-                    }
                 }
-               
-                // categeory three
-                VStack(alignment: .leading) {
-                    Text("Men_Accessories")
-                        .font(.headline)
-                        .padding(.horizontal)
-
-                    ScrollView(.horizontal, showsIndicators: true) {
-                        HStack(spacing: 10) {
-                            ForEach(0..<10) { index in
-                                Image("Men_Accessories") // Use actual image names for each category
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 200, height: 200)
-                                    // Use actual image names or URLs for each category
-                                    // Example: Image("clothesImageName")
-                            }
-                        }
-                        .padding(.horizontal)
-                    }
-                }
-               
-                
+            }
             
         }
-       
     }
-}
-
-
-
-}
-
-
-
-
-struct WomenCategoryView: View {
-    let categories = ["men", "women", "kid"] // Define your categories here
-
-    var body: some View {
-        ScrollView {
-            VStack(alignment: .leading) {
-            
-                    
-                    VStack(alignment: .leading) {
-                        Text("Women CLothing")
-                            .font(.headline)
-                            .padding(.horizontal)
-
-                        ScrollView(.horizontal, showsIndicators: true) {
-                            HStack(spacing: 10) {
-                                ForEach(0..<10) { index in
-                                    Image("Women CLothing") // Use actual image names for each category
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 200, height: 200)
-                                        // Use actual image names or URLs for each category
-                                        // Example: Image("clothesImageName")
-                                }
-                            }
-                            .padding(.horizontal)
-                        }
-                    }
-                   
-                // Second Category
-                VStack(alignment: .leading) {
-                    Text("Women shoes")
-                        .font(.headline)
-                        .padding(.horizontal)
-
-                    ScrollView(.horizontal, showsIndicators: true) {
-                        HStack(spacing: 10) {
-                            ForEach(0..<10) { index in
-                                Image("Women shoes") // Use actual image names for each category
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 200, height: 200)
-                                    // Use actual image names or URLs for each category
-                                    // Example: Image("clothesImageName")
-                            }
-                        }
-                        .padding(.horizontal)
-                    }
-                }
-               
-                // categeory three
-                VStack(alignment: .leading) {
-                    Text("Women Accessories")
-                        .font(.headline)
-                        .padding(.horizontal)
-
-                    ScrollView(.horizontal, showsIndicators: true) {
-                        HStack(spacing: 10) {
-                            ForEach(0..<10) { index in
-                                Image("Women Accessories") // Use actual image names for each category
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 200, height: 200)
-                                    // Use actual image names or URLs for each category
-                                    // Example: Image("clothesImageName")
-                            }
-                        }
-                        .padding(.horizontal)
-                    }
-                }
-               
-                
-            
-        }
-       
-    }
-}
-
-
-
-}
-
-
-
-
-struct KidCategoryView: View {
-    let categories = ["men", "women", "kid"] // Define your categories here
-
-    var body: some View {
-        ScrollView {
-            VStack(alignment: .leading) {
-            
-                    
-                    VStack(alignment: .leading) {
-                        Text("Kids Clothing")
-                            .font(.headline)
-                            .padding(.horizontal)
-
-                        ScrollView(.horizontal, showsIndicators: true) {
-                            HStack(spacing: 10) {
-                                ForEach(0..<10) { index in
-                                    Image("Kids Clothing") // Use actual image names for each category
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 200, height: 200)
-                                        // Use actual image names or URLs for each category
-                                        // Example: Image("clothesImageName")
-                                }
-                            }
-                            .padding(.horizontal)
-                        }
-                    }
-                   
-                // Second Category
-                VStack(alignment: .leading) {
-                    Text("Kids Shoes")
-                        .font(.headline)
-                        .padding(.horizontal)
-
-                    ScrollView(.horizontal, showsIndicators: true) {
-                        HStack(spacing: 10) {
-                            ForEach(0..<10) { index in
-                                Image("Kids Shoes") // Use actual image names for each category
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 200, height: 200)
-                                    // Use actual image names or URLs for each category
-                                    // Example: Image("clothesImageName")
-                            }
-                        }
-                        .padding(.horizontal)
-                    }
-                }
-               
-                // categeory three
-                VStack(alignment: .leading) {
-                    Text("Kids Accessories")
-                        .font(.headline)
-                        .padding(.horizontal)
-
-                    ScrollView(.horizontal, showsIndicators: true) {
-                        HStack(spacing: 10) {
-                            ForEach(0..<10) { index in
-                                Image("Kids Accessories") // Use actual image names for each category
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 200, height: 200)
-                                    // Use actual image names or URLs for each category
-                                    // Example: Image("clothesImageName")
-                            }
-                        }
-                        .padding(.horizontal)
-                    }
-                }
-               
-                
-            
-        }
-       
-    }
-}
-
-
-
+    
 }
