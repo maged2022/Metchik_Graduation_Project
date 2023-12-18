c7c0cfd63f89619dbfb99384728cb71889aa538d
maged mohamed <maged12345@Github.com>
2023-12-18 15:25:06 +0200

    adding DiscountView

--- Metchik/Home Screen/Views/SubCategoryView.swift
+++ Metchik/Home Screen/Views/SubCategoryView.swift
@@ -29,7 +29,7 @@ struct SubCategoryView: View {
                                 .onTapGesture {
                                     categorie = item
                                 }
-
+                            
                             Divider()
                                 .frame(width: 40, height: 3)
                                 .background(.red)
@@ -37,36 +37,31 @@ struct SubCategoryView: View {
                         }
                     }
                 }
-               
-                    VStack(alignment: .leading) {
-                        ScrollView(.horizontal, showsIndicators: true) {
-                            HStack(spacing: 10) {
-                                ForEach(subCategoryViewModel.subCategories) { subCategory in
+                
+                ScrollView(.horizontal, showsIndicators: true) {
+                    HStack(spacing: 10) {
+                        ForEach(subCategoryViewModel.subCategories) { subCategory in
+                            NavigationLink {
+                                // New Screen
+                                ProductView(selectedSubcategory: subCategory)
+                            } label: {
+                                VStack {
+                                    Image("\(categorie) \(subCategory.name)")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame( height: 200)
+                                        .clipShape(RoundedRectangle(cornerRadius: 10))
                                     
-                                    NavigationLink {
-                                        // New Screen
-                                        ProductView(selectedSubcategory: subCategory)
-                                    } label: {
-                                        VStack {
-                                            Image("\(categorie) \(subCategory.name)")
-                                                .resizable()
-                                                .aspectRatio(contentMode: .fit)
-                                                .frame( height: 200)
-                                                .clipShape(RoundedRectangle(cornerRadius: 10))
-                                            
-                                            Text("\(subCategory.name)")
-                                                .font(.headline)
-                                        }
-                                        
-                                    }
-                                    
-                                    
+                                    Text("\(subCategory.name)")
+                                        .font(.headline)
                                 }
                             }
                         }
-                        
-                      
                     }
+                }
+                
+                // Discount Section
+                DiscountView()
                 
             }
             .navigationTitle(categorie)
@@ -77,13 +72,72 @@ struct SubCategoryView: View {
             .ignoresSafeArea()
             .opacity(0.2))
     }
-    
 }
 
 
 struct CategoryView_Previews: PreviewProvider {
-    
     static var previews: some View {
         SubCategoryView(categorie: "Men")
+    }
+}
+
+
+struct Discount: Identifiable {
+    let id: UUID = UUID()
+    let imageName: String
+    let oldPrice: Double
+    let newPrice: Double
+}
+
+struct DiscountView: View {
+    
+    let discountList: [Discount] = [
+        Discount(imageName: "discount_image1", oldPrice: 514.12, newPrice: 354.21),
+        Discount(imageName: "discount_image1", oldPrice: 514.12, newPrice: 354.21),
+        Discount(imageName: "discount_image1", oldPrice: 514.12, newPrice: 354.21),
+        Discount(imageName: "discount_image1", oldPrice: 514.12, newPrice: 354.21),
+        Discount(imageName: "discount_image1", oldPrice: 514.12, newPrice: 354.21),
+        Discount(imageName: "discount_image1", oldPrice: 514.12, newPrice: 354.21),
+    ]
+    
+    var body: some View {
+        ScrollView {
+            
+            VStack {
+                
+                Text("The End of Season Sale is HERE! 🔥 Don’t miss the chance to shop all your Metchik Favorite at discount price with up to 50% OFF.Shop now ")
+                
+                 // Image Discount logo
+                Image("discount_image")
+                    .resizable()
+                    .scaledToFit()
+                
+                
+                // Image
+                ForEach(discountList) { item in
+                    VStack{
+                        Image(item.imageName)
+                            .resizable()
+                            .scaledToFit()
+                        VStack {
+                            
+                            Text("$\(String(format: "%.2f", item.newPrice))")
+                                .foregroundColor(.gray)
+                                .padding(.top, 4)
+                            
+                            Text("$\(String(format: "%.2f", item.oldPrice))")
+                                .foregroundColor(.gray)
+                                .padding(.top, 4)
+                                .strikethrough() 
+                        }
+                    }
+                }
+                
+                
+                
+            }
+            
+          
+        }
     }
 }
