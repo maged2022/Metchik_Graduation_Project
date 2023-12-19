2ba55db6883e3b4a90567fef6d40771a26df0954
maged mohamed <maged12345@Github.com>
2023-12-19 02:17:00 +0200

    Editing description and add read more and show less

--- Metchik/Home Screen/Views/DiscountView.swift
+++ Metchik/Home Screen/Views/DiscountView.swift
@@ -10,13 +10,26 @@ import SwiftUI
 struct DiscountView: View {
     
     @StateObject private var discountViewModel = DiscountViewModel()
+    let description: String = "The End of Season Sale is HERE! 🔥🔥 Don’t miss the chance to shop all your Metchik Favorites at discounted prices with up to 50% OFF. Shop now 🛍"
+    @State var compressDescription: Bool = false
     
     var body: some View {
         ScrollView {
-            
             VStack(spacing: 20) {
-                
-                Text("The End of Season Sale is HERE! 🔥 Don’t miss the chance to shop all your Metchik Favorite at discount price with up to 50% OFF.Shop now ")
+                VStack(alignment: .leading) {
+                    Text(description)
+                        .font(.title2)
+                        .lineLimit(compressDescription ? nil : 2)
+                    Button {
+                        withAnimation(.easeInOut) {
+                            compressDescription.toggle()
+                        }
+                    } label: {
+                        Text( compressDescription ? "Show less" : "Read more" )
+                            .foregroundColor(.blue)
+                            .underline()
+                    }
+                }
                 
                 Spacer().frame(height: 20)
                 // Image Discount logo
@@ -28,17 +41,13 @@ struct DiscountView: View {
                 // Image
                 ForEach(discountViewModel.discounts) { item in
                     VStack{
-                        
                         Image(item.imageName)
                             .resizable()
                             .scaledToFit()
-                        
                         VStack(spacing: 5) {
-                            
                             Text("$\(String(format: "%.2f", item.oldPrice))")
                                 .foregroundColor(.gray)
                                 .strikethrough()
-                            
                             Text("$\(String(format: "%.2f", item.newPrice))")
                                 .foregroundColor(.gray)
                         }
@@ -52,7 +61,6 @@ struct DiscountView: View {
         }
     }
 }
-
 
 struct DiscountView_Previews: PreviewProvider {
     static var previews: some View {
