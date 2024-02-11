fe21e3937273418cbbae1f44e8d6310a3d423d72
hassanSayed0 <111157516+hassanSayed0@users.noreply.github.com>
2024-02-11 08:07:39 +0200

    update CartRowView design

--- Metchik/Home Screen/Views/BasketView/CartRowView.swift
+++ Metchik/Home Screen/Views/BasketView/CartRowView.swift
@@ -15,54 +15,52 @@ struct CartRowView: View {
     var index: Int
     
     var body: some View {
-        HStack(alignment: .center, spacing: 15) {
+        HStack(alignment: .bottom, spacing: 6) {
             Image(product.images[0])
                 .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 110)
+                .scaledToFill()
+                .frame(width: 80,height: 80)
                 .cornerRadius(10)
-                .padding(.leading)
-                .shadow(color: .black.opacity(0.1), radius: 10, x: 5, y: 5)
-                .shadow(color: .black.opacity(0.1), radius: 10, x: -5, y: -5)
-            
-            VStack (alignment: .leading, spacing: 3) {
-                
-                Text(product.name)
-                    .font(.system(size: 15))
-                    .bold()
-                
-                Text(String(format: "%.2f L.E", product.discountPrice))
-                    .font(.system(size: 13))
-                    .bold()
-                
-                HStack {
-                    Text("Color:  Red ")
-                        .font(.system(size: 13))
-                        .bold()
-                    
-                    Spacer()
-                    Button ( action: {
-                        withAnimation {
-                            productViewModel.removeProduct(index: index)
 
-                        }
-                        
-                    }){
-                        Image(systemName: "trash")
-                            .foregroundColor(Color(hue: 1.0, saturation: 0.89, brightness: 0.835))
-                            .padding(.horizontal)
-                    }
-                }
+            VStack(alignment: .leading,spacing: 2) {
+                Text("Roller Rabbit")
+                    .font(.poppins(.semiBold, size: 14))
                 
-                Text("Size:  S")
-                    .font(.system(size: 13))
-                    .bold()
-
+                Text("Vado Odelle Dress")
+                    .font(.poppins(.regular, size: 11))
+                    .foregroundStyle(Color.themeColor.secondaryLabelColor)
+                    .padding(.bottom,12)
+                Text("$198.00")
+                    .font(.poppins(.bold, size: 14))
+                    .padding(.bottom,4)
             }
-            .padding(.bottom, 20)
-            .frame(width: 250)
+            .frame(maxWidth: .infinity,alignment: .leading)
+            
+            HStack {
+                Button {
+                } label: {
+                    Image(systemName: "minus")
+                }
+                Text("1")
+                Button {
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .font(.poppins(.regular, size: 14))
+            .foregroundStyle(Color.themeColor.primaryLabelColor)
+            .padding(.vertical,5)
+            .padding(.horizontal,12)
+            .background(Color.themeColor.backgroundSearchColor)
+            .cornerRadius(30)
+            .padding(.leading,10)
+            .padding(.trailing,5)
+           
         }
+        .padding(10)
+        .cornerRadius(13)
     }
+        
 }
 
 struct CartRowView_Previews: PreviewProvider {
@@ -71,8 +69,7 @@ struct CartRowView_Previews: PreviewProvider {
         let productViewModel = ProductViewModel()
         
         return CartRowView(productViewModel: productViewModel, product: product, index: 0)
-//            .previewLayout(.fixed(width: 300, height: 100))
-//            .padding()
+
     }
 }
 
