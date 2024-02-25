a1e4be15fddb36449ff9f6c2493ffa54f948b01a
Hassan Sayed <111157516+hassanSayed0@users.noreply.github.com>
2024-02-25 07:21:07 +0200

    update domain layer for productDetail

--- Metchik/Domain/Repositories/ProductDetailRepositories.swift
+++ Metchik/Domain/Repositories/ProductDetailRepositories.swift
@@ -5,8 +5,12 @@
 //  Created by Hassan on 24/02/2024.
 //
 
-import Foundation
+import SwiftUI
 import Combine
 protocol ProductDetailRepositories {
-    func getProductDetail(by id : String) -> AnyPublisher<ProductDetail, Never>
+    func fetchProductDetail(by id : String)
+    func getProductDetail() -> AnyPublisher<ProductDetail, Never>
+    func getAvilableSizes() -> AnyPublisher<[ProductSizes], Never>
+    func getAvilableColors (forSize selectedSize: ProductSizes) -> AnyPublisher<[Color], Never>
+    func getMaxAvilableProducts(size: ProductSizes, color: Color) -> AnyPublisher<Int, Never>
 }
--- Metchik/Domain/UseCases/ProductDetailUseCase.swift
+++ Metchik/Domain/UseCases/ProductDetailUseCase.swift
@@ -5,14 +5,51 @@
 //  Created by Hassan on 24/02/2024.
 //
 
-import Foundation
 import Combine
+import SwiftUI
 
 class ProductDetailUseCase: ProductDetailRepositories, ObservableObject {
     @Published private var repo = ProductSourceDetailRepositoriesImpl()
+    @Published private var productDetail: ProductDetail = .mockData
 
-    func getProductDetail(by id : String) -> AnyPublisher<ProductDetail, Never> {
-        repo.getProductSourceDetail(by: id).map { $0.toProductDetail() }
+    private var cancellables = Set<AnyCancellable>()
+
+    func fetchProductDetail(by id : String) {
+        repo.getProductSourceDetail(by: id)
+            .map { $0.toProductDetail() }.sink {[weak self] productDetail in
+                self?.productDetail = productDetail
+            }
+            .store(in: &cancellables)
+    }
+    
+    func getProductDetail() -> AnyPublisher<ProductDetail, Never> {
+        $productDetail.eraseToAnyPublisher()
+    }
+
+    func getAvilableSizes() -> AnyPublisher<[ProductSizes], Never> {
+        return $productDetail.map { $0.productAttribute.map {$0.sizes}} .eraseToAnyPublisher()
+    }
+    
+    func getAvilableColors(forSize selectedSize: ProductSizes) -> AnyPublisher<[Color], Never> {
+        return $productDetail
+            .map({$0.productAttribute
+                    .first {$0.sizes == selectedSize }
+                    .map {$0.colors} ?? []
+            })
             .eraseToAnyPublisher()
+    }
+    
+    func getMaxAvilableProducts(size: ProductSizes, color: Color) -> AnyPublisher<Int, Never> {
+        return $productDetail
+              .map { productDetail in
+                  guard let sizeAttribute = productDetail.productAttribute
+                      .filter({ $0.sizes == size })
+                      .first,
+                      let colorIndex = sizeAttribute.colors.firstIndex(where: { $0 == color }) else {
+                          return 0
+                  }
+                  return sizeAttribute.avaliableInStok[colorIndex]
+              }
+              .eraseToAnyPublisher()
     }
 }
