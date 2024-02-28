d6fb84a53f5167588cecb5ca9b4b38d314a3f89c
Hassan Sayed <111157516+hassanSayed0@users.noreply.github.com>
2024-02-28 07:00:48 +0200

    refactor subcategory view

--- Metchik/Screens/Home/SubCategory/SubCategoryViewModel.swift
+++ Metchik/Screens/Home/SubCategory/SubCategoryViewModel.swift
@@ -4,14 +4,43 @@
 //
 //  Created by maged on 18/12/2023.
 //
-/*
+
 import Foundation
+import Combine
 
 class SubCategoryViewModel: ObservableObject {
-    @Published var subCategories: [SubCategory] = [
-        SubCategory(name: "Shoes"),
-        SubCategory(name: "Clothing"),
-        SubCategory(name: "Accessories")
-    ]
+    private let productUseCase: ProductRepositories = ProductUseCase()
+    private var cancellables = Set<AnyCancellable>()
+    
+    let category: String
+    @Published var subCategories: [String] = [] {
+        didSet {
+            updateProducts()
+        }
+    }
+    @Published var products: [String: [Product]] = [:]
+    init(category: String) {
+        self.category = category
+        updateSubCategories()
+    }
+    private func updateSubCategories() {
+        productUseCase.getSubCategories(category: category)
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] subCategories in
+                self?.subCategories = subCategories
+            }
+            .store(in: &cancellables)
+    }
+
+    private func updateProducts() {
+        productUseCase.getProducts(category: category, subCategories: subCategories)
+            .sink { [weak self] product in
+                self?.products = product
+            }
+            .store(in: &cancellables)
+    }
+    func getProductsCount(for subCategory: String) -> Int {
+        products[subCategory]?.count ?? 1
+    }
+    
 }
-*/
