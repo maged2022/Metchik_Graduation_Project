5d54e23cd1580c385f497d4d57ffbd5f4076ad7f
Hassan Sayed <111157516+hassanSayed0@users.noreply.github.com>
2024-02-28 21:05:20 +0200

    create file for extension

--- Metchik/Domain/Entities/Product.swift
+++ Metchik/Domain/Entities/Product.swift
@@ -20,29 +20,10 @@ struct Product: Identifiable {
     static let mockData: Product = Product(id: "1",
                                            name: "test",
                                            shortDescription: "shortDescription",
-                                           price: 44,
+                                           price: 44.7,
                                            discountPercentage: 32,
-                                           mainImage:  AssetImage.images.swiftUIImage ,
+                                           mainImage:  AssetImage.discountImage2.swiftUIImage ,
                                            category: "",
                                            subCategory: "")
     
-}
-
-extension Array where Element == ProductSource {
-    
-    func toProducts() -> [Product] {
-            return self.map { source in
-                Product(
-                    id: source.id, // Replace with the actual id property of ProductSource
-                    name: source.name,
-                    shortDescription: source.shortDescription,
-                    price: source.price,
-                    discountPercentage: source.discountPercentage,
-//                    isFavorite: source.isFavorite, // Assuming isFavorite is a property of ProductSource
-                    mainImage: ImageAsset(name: source.mainImage).swiftUIImage,
-                    category: source.category,
-                    subCategory: source.subCategory
-                )
-            }
-        }
 }
