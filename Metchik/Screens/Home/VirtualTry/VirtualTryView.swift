497b4882aed98a13bc8db7a41a9852685c5dbd89
maged mohamed <maged12345@Github.com>
2024-05-01 13:21:44 +0300

    Refactoring VisualTryView

--- Metchik/Screens/Home/VirtualTry/VirtualTryView.swift
+++ Metchik/Screens/Home/VirtualTry/VirtualTryView.swift
@@ -12,29 +12,44 @@ struct VirtualTryView: View {
 
     var body: some View {
         VStack {
+            Spacer().frame(height: 10)
             HStack {
                 AsyncImage(url: virtualTryViewModel.productImageURL) { image in
                     image
                         .resizable()
-                        .frame(maxWidth: .infinity, minHeight: 200, maxHeight: 430)
+                        .frame(height: 180)
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(8)
                 } placeholder: {
                     ProgressView()
                 }
                 Image(uiImage: virtualTryViewModel.personImage)
                     .resizable()
+                    .frame(height: 180)
                     .aspectRatio(contentMode: .fit)
-                    .navigationBarTitle("Selected Image")
-                    .edgesIgnoringSafeArea(.all)
+                    .cornerRadius(8)
             }
+            
+            .frame(maxWidth: .infinity)
+            .padding(.horizontal)
+            
+            
+            Spacer().frame(height: 40)
            
             AsyncImage(url: virtualTryViewModel.virtualImageURL) { image in
                 image
                     .resizable()
                     .frame(maxWidth: .infinity, minHeight: 200, maxHeight: 430)
             } placeholder: {
-                ProgressView()
+                ProgressView("ML Processing Image")
+                    .padding()
             }
+            Spacer()
+           
         }
+        .background(
+            Color(red: 220 / 255, green: 224 / 255, blue: 225 / 255)
+        )
     }
 }
 
