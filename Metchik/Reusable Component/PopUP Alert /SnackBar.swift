243ddbec6ec3ab0200e418d2a1f933e6eec2d441
maged mohamed <maged12345@Github.com>
2024-05-15 18:19:33 +0300

    add popUp view

---
+++ Metchik/Reusable Component/PopUP Alert /SnackBar.swift
@@ -0,0 +1,72 @@
+//
+//  SnackBar.swift
+//  Metchik
+//
+//  Created by maged on 15/05/2024.
+//
+
+import SwiftUI
+
+struct SnackBar: View {
+    
+    let title: String
+    let message: String
+    let buttonTitle: String
+    var onClick: () -> Void
+    
+    let buttonWidth: CGFloat = 100 // Adjust as needed
+    
+    var body: some View {
+        VStack {
+            
+            Image("alertIcon")
+                .resizable()
+                .frame(width: 33.3, height: 30)
+                .foregroundColor(.white)
+                .frame(width: 80, height: 80)
+                .background(Color.black.cornerRadius(40))
+                .clipShape(Circle())
+                .padding()
+            
+            Text(title)
+                .font(.poppins(.bold, size: 24))
+            
+            Text(message)
+                .font(.poppins(.light, size: 14))
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 50)
+
+            
+          
+                Button(action: {
+                    self.onClick()
+                }) {
+                    Text(buttonTitle)
+                        .foregroundColor(.white)
+                        .font(.poppins(.boldItalic, size: 16))
+                        .padding(.horizontal, 25)
+                        .frame( height: 50)
+                        .background(
+                            RoundedRectangle(cornerRadius: 30)
+                                .foregroundColor(.black)
+                        )
+                        
+                }
+        }
+        .padding()
+        .frame(maxWidth: .infinity)
+        .background(Color.white)
+        .cornerRadius(10)
+        .shadow(radius: 5)
+        .padding(15)
+    }
+}
+
+
+struct SnackBar_Previews: PreviewProvider {
+    static var previews: some View {
+        SnackBar(title: "Successful!", message: "You have successfully your Confirm Payment send!", buttonTitle: "continue shopping", onClick: {})
+    }
+}
+
