01379f563af2449561f56fc27a12c077af587d73
maged mohamed <maged12345@Github.com>
2024-05-22 00:39:33 +0300

    adding sort section and other section

--- Metchik/Screens/Home/Filter/FilterView.swift
+++ Metchik/Screens/Home/Filter/FilterView.swift
@@ -8,11 +8,16 @@
 import SwiftUI
 
 struct FilterView: View {
-  
+    
     let spacingBetweenColumns: CGFloat = 5
     let viewModel: FilterViewModel = FilterViewModel()
+    @State private var selectedItem: String = "Dresses"
+    
+    let dayList: [String] = ["New Today", "New This Week", "Top Sellers"]
+    @State var dayListSelected = "New Today"
+    
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .center, spacing: 20){
             HStack {
                 Text("Categories")
                     .font(.poppins(.bold, size: 20))
@@ -31,12 +36,48 @@ struct FilterView: View {
                     spacing: 20
                 ) {
                     ForEach(viewModel.categories, id: \.self) { category in
-                        CategoryButton(category: category, isSelected: false) {
+                        CategoryButton(category: category, isSelected: selectedItem == category) {
                             print("\(category) button clicked")
+                            selectedItem = category
                         }
                     }
                 }
             }
+            .frame(height: 140 )
+            
+            
+            // Price Range Section
+            HStack {
+                Text("Price Range")
+                    .font(.poppins(.bold, size: 20))
+                Spacer()
+            }
+            
+            
+            // Sort by Section
+            HStack {
+                Text("Sort By")
+                    .font(.poppins(.bold, size: 20))
+                Spacer()
+            }
+            
+            HStack {
+                ForEach(dayList, id: \.self) { day in
+                    CategoryButton(category: day, isSelected: dayListSelected == day) {
+                        print("\(day) button Clicked")
+                        dayListSelected = day
+                    }
+                }
+            }
+            
+            // Rating Section
+            HStack {
+                Text("Rating")
+                    .font(.poppins(.bold, size: 20))
+                Spacer()
+            }
+            
+            Spacer()
         }
         .padding(.horizontal, 10)
     }
